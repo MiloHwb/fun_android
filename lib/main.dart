baaf399b8582a7ca8ab2b5c975838c255efaad8b
hwbmilo <hwbmilo@gmail.com>
2019-08-29 22:19:24 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+///项目源地址 https://github.com/phoenixsky/fun_android_flutter
+void main(){
+  //初始化操作，单例，负责创建WidgetsFlutterBinding对象
+  WidgetsFlutterBinding.ensureInitialized();
+};
+
